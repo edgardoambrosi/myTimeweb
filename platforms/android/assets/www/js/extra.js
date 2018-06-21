1ac0c0f388796d6e3d6ed64798264a079cf5c737
AMBROSI EDGARDO <edgardo.ambrosi@cnr.it>
2018-06-21 08:35:04 +0200

    Aggiorno

--- platforms/android/assets/www/js/extra.js
+++ platforms/android/assets/www/js/extra.js
@@ -1083,11 +1083,8 @@ $( document ).ready(function() {
 		$('#contatti_call').click(function(){
         	$('.menu-act').trigger('hover')				
         	$('.overlay-hide').trigger('click')
-	        $('#make_a_call').click(function(){	
-	        	console.log('3286755538');
-		        window.plugins.CallNumber.callNumber(onSuccess, onError, 3286755538, bypassAppChooser);	
-		    });    
-    		$('#form_contatti').show()	    	
+	        	
+    		$('#form_contatti').show();	    	
 		});		
 
 		$('#informazioni_call').click(function(){
