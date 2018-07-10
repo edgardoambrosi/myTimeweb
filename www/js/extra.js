0c5be5edc81ab1154a059e450295b745d38649f2
AMBROSI EDGARDO <edgardo.ambrosi@cnr.it>
2018-07-10 15:45:08 +0200

    Aggiorno

--- www/js/extra.js
+++ www/js/extra.js
@@ -1616,10 +1616,36 @@ $( document ).ready(function() {
 
 						        var blob = new Blob([data], { type: 'text/plain' });
 						        fileWriter.write(blob);
-						    }, errorHandler.bind(null, fileName));
-						}, errorHandler.bind(null, fileName));
-					}, errorHandler.bind(null, fileName));
+						    }, log.errorHandler.bind(null, fileName));
+						}, log.errorHandler.bind(null, fileName));
+					}, log.errorHandler.bind(null, fileName));
 			},
+			errorHandler:function(fileName, e) {  
+				var msg = '';
+
+				switch (e.code) {
+					case FileError.QUOTA_EXCEEDED_ERR:
+						msg = 'Storage quota exceeded';
+						break;
+					case FileError.NOT_FOUND_ERR:
+						msg = 'File not found';
+						break;
+					case FileError.SECURITY_ERR:
+						msg = 'Security error';
+						break;
+					case FileError.INVALID_MODIFICATION_ERR:
+						msg = 'Invalid modification';
+						break;
+					case FileError.INVALID_STATE_ERR:
+						msg = 'Invalid state';
+						break;
+					default:
+						msg = 'Unknown error';
+						break;
+				};
+
+				console.log('Error (' + fileName + '): ' + msg);
+			},					
 			info:function(mess){	
 				var g=setInterval(function(){
 					if ( log.logDirectory != "" ){
