b166a01b966020e46c3028aac2f1e6e263d10468
AMBROSI EDGARDO <edgardo.ambrosi@cnr.it>
2018-07-11 08:45:50 +0200

    Aggiorno

--- www/js/extra.js
+++ www/js/extra.js
@@ -1601,55 +1601,14 @@ $( document ).ready(function() {
 					}	
 				},1000)
 			},
+			writeHandler:function(fileEntry){
+				console.log("QUI   "+fileEntry.name)
+			},
 			writeToFile:function(fileName,data){	
 					log.logFileName=fileName;
 					data = JSON.stringify(data, null, '\t');
-					window.resolveLocalFileSystemURL(log.logDirectory, log.writeHandler, log.errorHandler) 
-				
+					window.resolveLocalFileSystemURL(log.logDirectory, log.writeHandler) 
 			},
-			writeHandler:function(dataDirectory){
-				directoryEntry.getFile(log.logFileName, { create: true }, function (fileEntry) {
-				    fileEntry.createWriter(function (fileWriter) {
-				        fileWriter.onwriteend = function (e) {
-				            // for real-world usage, you might consider passing a success callback
-				            console.log('Write of file "' + log.logFileName + '"" completed.');
-				        };
-				        fileWriter.onerror = function (e) {
-				            // you could hook this up with our global error handler, or pass in an error callback
-				            console.log('Write failed: ' + e.toString());
-				        };
-
-				        var blob = new Blob([data], { type: 'text/plain' });
-				        fileWriter.write(blob);
-				    }, log.errorHandler(log.logFileName,a));
-				}, log.errorHandler(log.logFileName,a));
-			},
-			errorHandler:function(fileName, e) {  
-				var msg = '';
-
-				switch (e.code) {
-					case FileError.QUOTA_EXCEEDED_ERR:
-						msg = 'Storage quota exceeded';
-						break;
-					case FileError.NOT_FOUND_ERR:
-						msg = 'File not found';
-						break;
-					case FileError.SECURITY_ERR:
-						msg = 'Security error';
-						break;
-					case FileError.INVALID_MODIFICATION_ERR:
-						msg = 'Invalid modification';
-						break;
-					case FileError.INVALID_STATE_ERR:
-						msg = 'Invalid state';
-						break;
-					default:
-						msg = 'Unknown error';
-						break;
-				};
-
-				console.log('Error (' + fileName + '): ' + msg);
-			},					
 			info:function(mess){	
 				var g=setInterval(function(){
 					if ( log.logDirectory != "" ){
